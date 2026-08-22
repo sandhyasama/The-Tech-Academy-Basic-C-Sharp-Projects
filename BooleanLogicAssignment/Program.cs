26b4e1b98e04226b96fd03495e1ae2029b21cdb8
sandhyasama <50727198+sandhyasama@users.noreply.github.com>
2026-08-22 16:33:03 -0400

    Add Boolean Logic Assignment Code
    For Basic three questions based insurance approval

---
+++ BooleanLogicAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿//display age question
+Console.WriteLine("What is your age?");
+//capture user input and convert to byte
+byte ageInput = Convert.ToByte(Console.ReadLine());
+
+//display DUI question
+Console.WriteLine("Have you ever had a DUI? (true/false)");
+//get the user input and convert to boolean
+bool duiInput = Convert.ToBoolean(Console.ReadLine());
+
+//display speeding tickets
+Console.WriteLine("How many speeding tickets do you have?");
+//get the user input and convert to byte
+byte ticketsInput = Convert.ToByte(Console.ReadLine());
+
+Boolean isQualified = (ageInput > 15) && (duiInput == false) && (ticketsInput <= 3);
+
+//write result message line
+Console.WriteLine("Qualified? \n" + isQualified);
+
+
