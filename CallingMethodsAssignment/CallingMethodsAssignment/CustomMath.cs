b0d27231c9fe8d08980e3f263ea6d82d8f9a09b8
sandhyasama <50727198+sandhyasama@users.noreply.github.com>
2026-09-21 22:15:06 -0400

    calling methods assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/CustomMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsAssignment
+{
+    //class containing methods to add, multiply, and subtract a number by 10
+    internal class CustomMath
+    {
+
+        //add number by 10
+        public int AddNumberByTen(int number)
+        {
+            return number + 10;
+        }
+
+        //multiply number by 10
+        public int MultiplyNumberByTen(int number)
+        {
+            return number * 10;
+        }
+
+        //subtract number by 10
+        public int SubtractNumberByTen(int number)
+        {
+            return number - 10;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using CallingMethodsAssignment;
+
+//ask the user for a number to perform math operations on
+Console.WriteLine("Hello, enter the number you want to perform math operations on:");
+//get the user input and convert it to an integer
+int userInput = Convert.ToInt32(Console.ReadLine());
+
+//instantiate the CustomMath class
+CustomMath customMath = new CustomMath();
+//perform addition
+int addedResult = customMath.AddNumberByTen(userInput);
+//display the result of addition
+Console.WriteLine($"Adding 10 to {userInput} gives: {addedResult}");
+
+//perform multiplication
+int multipliedResult = customMath.MultiplyNumberByTen(userInput);
+//display the result of multiplication
+Console.WriteLine($"Multiplying {userInput} by 10 gives: {multipliedResult}");
+
+//perform subtraction
+int subtractedResult = customMath.SubtractNumberByTen(userInput);
+//display the result of subtraction
+Console.WriteLine($"Subtracting 10 from {userInput} gives: {subtractedResult}");
