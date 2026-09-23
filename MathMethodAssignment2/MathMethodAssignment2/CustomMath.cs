3f0ad40303de69b4daf0038b04c919232b61a07e
sandhyasama <50727198+sandhyasama@users.noreply.github.com>
2026-09-22 20:45:28 -0400

    Method Assignment submission

---
+++ MathMethodAssignment2/MathMethodAssignment2/CustomMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathMethodAssignment2
+{
+    internal class CustomMath
+    {
+        public void performAddition(int num1, int num2) {
+            int result = num1 + 20;
+            Console.WriteLine("Second integer value is " + num2);
+
+        }
+    }
+}
---
+++ MathMethodAssignment2/MathMethodAssignment2/Program.cs
@@ -0,0 +1,10 @@
+﻿using MathMethodAssignment2;
+
+//instantiate the CustomMath class
+CustomMath customMath = new CustomMath();
+
+//call the method with two integers
+customMath.performAddition(10, 30);
+
+//call the method with parameter names
+customMath.performAddition(num1: 15, num2: 25);
