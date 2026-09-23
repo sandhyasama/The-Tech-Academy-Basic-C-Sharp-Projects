644ab56a76329c90007731bedae288024dbfca67
sandhyasama <50727198+sandhyasama@users.noreply.github.com>
2026-09-22 20:38:50 -0400

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/CustomMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment
+{
+    internal class CustomMath
+    {
+        //method with second parameter with default value i.e optional parameter
+        public int addTwoNumbers(int num1, int num2 = 5)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿//ask the user to enter first number
+using MethodAssignment;
+
+Console.WriteLine("please input first number");
+//grab and convert the input to an integer
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+//ask the user to enter second number
+Console.WriteLine("please input second number, please note that this is optional, you do not need to enter");
+string secondNumberInput = Console.ReadLine();
+
+//instantiate the CustomMath class
+CustomMath customMath = new CustomMath();
+
+//use try block to parse the second number input and check if it is valid
+//if the second number input is not empty, parse it and call the addTwoNumbers method with both parameters
+//if second number input is empty, catch block will be hit
+try
+{
+    //convert the second number input to an integer
+    int secondNumber = Convert.ToInt32(secondNumberInput);
+    //call the addTwoNumbers method with both parameters
+    int result = customMath.addTwoNumbers(firstNumber, secondNumber);
+    //display the result
+    Console.WriteLine($"The result of adding {firstNumber} and {secondNumber} is: {result}");
+
+}
+//handle exception if the second number input is not a valid integer or empty to call the method with just one parameter
+catch (Exception ex) {
+    //proceed with only the first number and use the default value for the second number
+    int result = customMath.addTwoNumbers(firstNumber);
+    //display the result
+    Console.WriteLine($"The result of adding {firstNumber} with default second parameter {result-firstNumber} is: {result}");
+
+}
+
+
+
+
+
+
+
+
+
